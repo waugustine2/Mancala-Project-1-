fc0e6bcb2843aeee10a122f5c95e2a73d72effa2
thekokobean <ookokomay@gmail.com>
2020-11-09 13:21:12 -0800

    Adds beggining javascript constants and state variables. Attached event listeners and cached elements

--- js/main.js
+++ js/main.js
@@ -1,23 +1,32 @@
 /*----- constants -----*/
-player
+const player = {
+    '1' : 'blue',
+    '-1' : 'yellow'
+};
 
 
 
 /*----- app's state (variables) -----*/
-let board;
-let turns;
+let turn;
 let moves;
 let scores;
+let stones;
 
 /*----- cached element references -----*/
-
+const pockets = document.querySelectorAll('.pocket');
+const message = document.getElementById('message')
+const playGame = document.getElementById('play')
 
 /*----- event listeners -----*/
-
+document.querySelector('board').addEventListener('click', handleMove);
+document.querySelector('play').addEventListener('click', init);
+document.querySelector('message').addEventListener('click', dispMsg);
+document.querySelector('rules').addEventListener('click', dispRules)
 
 /*----- functions -----*/
 init();
 
 render() {
+    renderStones();
     ;
 }
