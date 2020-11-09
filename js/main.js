f3583dc6e307eb05d15166956ebfdd8558fd57a6
thekokobean <ookokomay@gmail.com>
2020-11-09 11:28:14 -0800

    Fixes up board in HTML and CSS

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*----- constants -----*/
+player
+
+
+
+/*----- app's state (variables) -----*/
+let board;
+let turns;
+let moves;
+let scores;
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+init();
+
+render() {
+    ;
+}
